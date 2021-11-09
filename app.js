6a7f7823fc6d219a97e053a1402c909e86cb3fb1
alrigo123 <alexestrada2804@gmail.com>
2021-11-09 17:08:40 -0500

    Add

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const path = require("path");
+const morgan = require('morgan');
 const app = express();
 
 const index = require('./routes/index');
@@ -9,12 +10,24 @@ let PORT = "2021";
 app.set('view engine', 'ejs');
 
 
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+//Dev
+app.use(morgan('dev'));
+
+
+// Static Routes
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //route index
 app.use('/',index)
 
 
+//Listen to port
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
