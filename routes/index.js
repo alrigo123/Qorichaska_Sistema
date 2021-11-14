771e4004243a7ddfe30b065941bede39db10da0b
alrigo123 <alexestrada2804@gmail.com>
2021-11-14 10:25:38 -0500

    HTML

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 const view_Controller = require('../controllers/view_controller')
 
-router.get('/', view_Controller.index);
-router.get('/reserva',(req, res) => {
-    res.render('reserva', { title: 'QORICHASKA' })
-  });
+router.get('/', view_Controller.index)
+router.get('/reserva', (req, res) => {
+  res.render('reserva', { title: 'QORICHASKA' })
+})
 
-module.exports = router;
+
+module.exports = router
